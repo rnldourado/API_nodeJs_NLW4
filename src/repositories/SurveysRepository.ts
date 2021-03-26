cc22d3500e6e97985251a13c3aee9d2bf3a446d7
Raniel <rnldourado@gmail.com>
2021-03-26 10:39:50 -0300

    pré fase de testes - Funcionando

---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Surveys } from "../models/Surveys";
+
+@EntityRepository(Surveys)
+class SurveysRepository extends Repository<Surveys> {
+
+}
+export { SurveysRepository };
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import { app } from "./app";
+
+
+app.listen(5555, () => console.log('Server is running on port 5555!'));
